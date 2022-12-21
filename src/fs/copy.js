cc4a7340e44c7108cc959612f3fd6f21af93fd59
EugeneTsalko <tefworkmail@gmail.com>
2022-12-21 18:51:42 +0300

    fix: fs/create solution add

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,22 +1,5 @@
-import fs from 'node:fs';
-import { getDirName, checkFileExists } from '../utils.js';
-import path from 'node:path';
-
 const copy = async () => {
-    const __dirname = getDirName(import.meta.url);
-    const __filename = path.join(__dirname, '/files/fresh.txt');
-    const text = 'I am fresh and young';
-
-    const isFileExists = await checkFileExists(__filename);
-
-    if (!isFileExists) {
-        fs.writeFile(__filename, text, (err) => {
-            if (err) throw err;
-        });
-    } else {
-        throw new Error ('FS operation failed');
-    }
-
+    // Write your code here 
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import fs from 'node:fs';
+import { getDirName, checkFileExists } from '../utils.js';
+import path from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+    const __dirname = getDirName(import.meta.url);
+    const __filename = path.join(__dirname, '/files/fresh.txt');
+    const text = 'I am fresh and young';
+
+    const isFileExists = await checkFileExists(__filename);
+
+    if (!isFileExists) {
+        fs.writeFile(__filename, text, (err) => {
+            if (err) throw err;
+        });
+    } else {
+        throw new Error ('FS operation failed');
+    }
+
 };
 
 await create();
