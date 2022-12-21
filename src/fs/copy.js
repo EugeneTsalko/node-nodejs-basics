3df231754ce43f967e0e5704b9cd937b117e6151
EugeneTsalko <tefworkmail@gmail.com>
2022-12-21 19:53:19 +0300

    feat: solve fs/copy; fix

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import { getDirName, checkFolderExists, checkFolderEmpty } from "../utils.js";
+import path from "node:path";
+import fs from "node:fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+    const __dirname = getDirName(import.meta.url);
+    const __files = path.join(__dirname, 'files');
+    const __filesCopy = path.join(__dirname, 'files_copy');
+
+    const isFolderExists = await checkFolderExists(__files);
+    const isFolderCopyExists = await checkFolderExists(__filesCopy);
+    const isCopyPossible = isFolderExists && !isFolderCopyExists;
+
+    if (isCopyPossible) {
+        fs.mkdir(__filesCopy);
+        const files = await fs.readdir(__files);
+
+        for (const file of files) {
+            fs.copyFile(path.join(__files, file), path.join(__filesCopy, file));
+        }
+    } else {
+        throw new Error('FS operation failed');
+    }
 };
 
 await copy();
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-import fs from 'node:fs';
+import fs from 'node:fs/promises';
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
@@ -7,9 +7,47 @@ function getDirName(url) {
   return dirname(__filename);
 }
 
-function checkFileExists(file) {
-  return fs.promises.access(file, fs.constants.F_OK).then(() => true).catch(() => false)
+async function checkFileExists(path) {
+  try {  
+    const isPathValid = await fs.access(path, fs.constants.F_OK).then(() => true).catch(() => false);
+
+    if (isPathValid) {
+      const stats = await fs.lstat(path);
+  
+      return stats.isFile();
+    }
+  
+    return false;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function checkFolderExists(path) {
+  try {
+    const isPathValid = await fs.access(path, fs.constants.F_OK).then(() => true).catch(() => false);
+
+    if (isPathValid) {
+      const stats = await fs.lstat(path);
+  
+      return stats.isDirectory();
+    }
+  
+    return false;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function checkFolderEmpty(path) {
+  try {
+    const files = await fs.readdir(path);
+    return !files.length;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 
-export { getDirName, checkFileExists };
+
+export { getDirName, checkFileExists, checkFolderExists, checkFolderEmpty };
