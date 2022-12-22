6a64a623733abe66b9cb1d065441850d27c7cf86
EugeneTsalko <tefworkmail@gmail.com>
2022-12-22 13:33:55 +0300

    feat: solve cli/env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+
+
 const parseEnv = () => {
-    // Write your code here 
+    const env = process.env;
+
+    for (const rssVar in env) {
+        if (env.hasOwnProperty(rssVar) && rssVar.toString().startsWith('RSS_')) {
+            console.log(`${rssVar}=${env[rssVar]}`)
+        }
+    }
 };
 
 parseEnv();
