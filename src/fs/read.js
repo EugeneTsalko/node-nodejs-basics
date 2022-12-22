a47de78c9038c5f0e5e73f6529d989368a052a9c
EugeneTsalko <tefworkmail@gmail.com>
2022-12-22 12:39:29 +0300

    feat: solve fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import { getDirName, checkFileExists } from "../utils.js";
+import path from "node:path";
+import fs from "node:fs/promises";
+
 const read = async () => {
-    // Write your code here 
+    const __filename = path.join(getDirName(import.meta.url), 'files/fileToRead.txt');
+    const isFileExists = await checkFileExists(__filename);
+
+    if (isFileExists) {
+        const content = await fs.readFile(__filename, { encoding: 'utf8' });
+        console.log(content);
+    } else throw new Error ('FS operation failed');
 };
 
 await read();
