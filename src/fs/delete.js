541a210d6eeeb0ee401e66186056a82849c5a394
EugeneTsalko <tefworkmail@gmail.com>
2022-12-22 12:32:30 +0300

    feat: solve fs/list

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import { getDirName, checkFileExists } from "../utils.js";
+import path from "node:path";
+import fs from "node:fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+    const __filename = path.join(getDirName(import.meta.url), 'files/fileToRemove.txt');
+    const isFileExists = await checkFileExists(__filename);
+
+    if( isFileExists) {
+        fs.rm(__filename);
+    } else {
+        throw new Error ('FS operation failed');
+    }
 };
 
 await remove();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { getDirName, checkFolderExists } from "../utils.js";
+import path from "node:path";
+import fs from "node:fs/promises";
+
 const list = async () => {
-    // Write your code here 
+    const __dirname = path.join(getDirName(import.meta.url), 'files');
+    const isFilesExists = await checkFolderExists(__dirname);
+
+    if (isFilesExists) {
+      const files = await fs.readdir(__dirname);
+      
+      for (const file of files) {
+        console.log(file);
+      }
+    } else {
+        throw new Error ('FS operation failed');
+    }
 };
 
 await list();
