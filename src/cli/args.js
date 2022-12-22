c717b9c630777dee2e840f5ad103b13db6882a3b
EugeneTsalko <tefworkmail@gmail.com>
2022-12-22 14:21:11 +0300

    feat: solve cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+
+    const pairs = args.reduce(function(result, value, index, array) {
+        if (index % 2 === 0)
+          result.push(array.slice(index, index + 2));
+        return result;
+      }, []);
+
+    for (const pair of pairs) {
+        console.log(`${pair[0].replace(/--/g, '')} is ${pair[1]}`);
+    }
 };
 
 parseArgs();
